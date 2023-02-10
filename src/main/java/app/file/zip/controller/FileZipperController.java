3bf3686c6fed6647537d3c2693b039588285ac32
jeevanreddy25 <124979353+jeevanreddy25@users.noreply.github.com>
2023-02-10 12:06:37 +0530

    changes
    changes

--- src/main/java/app/file/zip/controller/FileZipperController.java
+++ src/main/java/app/file/zip/controller/FileZipperController.java
@@ -1,36 +1,33 @@
 package app.file.zip.controller;
 
-import java.io.IOException;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 import app.file.zip.service.FileZipperService;
 import lombok.extern.slf4j.Slf4j;
 
 
 @Slf4j
-@Controller
+@RestController
 public class FileZipperController {
 	
 	@Autowired
 	FileZipperService fileZipperService;
 	
 	@GetMapping(value = {"/","/home"})
-	public String homePage() {
-	    return "index";
+	public ModelAndView homePage() {
+		ModelAndView mo = new ModelAndView();
+		mo.setViewName("index");
+	    return mo;
 	}
 
-	@PostMapping("/index")
-	public void getGeneralFile(@RequestParam("file") MultipartFile file,Model model) throws IOException{
-		log.info("Api Called through html");
-		fileZipperService.convertToZipFile(file);
-		model.addAttribute("res", "Downnload Zip File");
-//		return "index";
-	}
+//	@PostMapping("/index")
+//	public void getGeneralFile(@RequestParam("file") MultipartFile file,Model model) throws IOException{
+//		log.info("Api Called through html");
+//		fileZipperService.convertToZipFile(file);
+//		model.addAttribute("res", "Downnload Zip File");
+////		return "index";
+//	}
 }
